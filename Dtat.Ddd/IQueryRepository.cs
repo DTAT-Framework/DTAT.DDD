c15badc34150478e4fecb0f83eb4e9097b7bfca0
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2021-08-23 22:43:14 +0430

    Making some changes!

--- Dtat.Ddd/IQueryRepository.cs
+++ Dtat.Ddd/IQueryRepository.cs
@@ -2,14 +2,14 @@
 {
 	public interface IQueryRepository<T> where T : IAggregateRoot
 	{
-		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>>
-			GetAllAsync(System.Threading.CancellationToken cancellationToken = default);
+		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>> GetAllAsync
+			(System.Threading.CancellationToken cancellationToken = default);
 
-		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>>
-			Find(System.Linq.Expressions.Expression<System.Func<T, bool>>
+		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>> GetSomeAsync
+			(System.Linq.Expressions.Expression<System.Func<T, bool>>
 			predicate, System.Threading.CancellationToken cancellationToken = default);
 
-		System.Threading.Tasks.Task<T> FindAsync
+		System.Threading.Tasks.Task<T> GetByIdAsync
 			(System.Guid id, System.Threading.CancellationToken cancellationToken = default);
 	}
 }
--- Dtat.Ddd/IRepository.cs
+++ Dtat.Ddd/IRepository.cs
@@ -22,14 +22,14 @@
 		System.Threading.Tasks.Task<bool> RemoveByIdAsync
 			(System.Guid id, System.Threading.CancellationToken cancellationToken = default);
 
-		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>>
-			GetAllAsync(System.Threading.CancellationToken cancellationToken = default);
+		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>> GetAllAsync
+			(System.Threading.CancellationToken cancellationToken = default);
 
-		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>>
-			Find(System.Linq.Expressions.Expression<System.Func<T, bool>>
+		System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<T>> GetSomeAsync
+			(System.Linq.Expressions.Expression<System.Func<T, bool>>
 			predicate, System.Threading.CancellationToken cancellationToken = default);
 
-		System.Threading.Tasks.Task<T> FindAsync
+		System.Threading.Tasks.Task<T> GetByIdAsync
 			(System.Guid id, System.Threading.CancellationToken cancellationToken = default);
 	}
 }
